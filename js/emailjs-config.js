a67ec2973ade4d74d98a6482e2bd796a826439b5
merkaz-shapira <eliaoo1478@gmail.com>
2026-04-29 15:50:35 +0300

    Delete js directory

--- js/emailjs-config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const EMAILJS_CONFIG = Object.freeze({
-  serviceId: "service_l30bbmn",
-  publicKey: "CD8JO1Tcg4AWRcm05",
-  templateId: "template_m7yh02g",
-  ownerEmail: "moriyamalul77@gmail.com"
-});
